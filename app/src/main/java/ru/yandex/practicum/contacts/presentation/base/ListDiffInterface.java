90fa467269041e78a11586da04854d908033fb65
lyeska6 <liz-on-chik@mail.ru>
2024-04-10 20:30:45 +0300

    переименованны переменные

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.contacts.presentation.base;
 
 public interface ListDiffInterface<T> {
 
-    boolean theSameAs(T smth);
+    boolean theSameAs(T anotherItem);
 
-    boolean equals(Object smth);
+    boolean equals(Object anotherItem);
 }
